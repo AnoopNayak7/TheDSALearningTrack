665c2e8ad9d5ef2f8963782a520b4e35c947964e
AnoopNayak7 <anoopnayak1@gmail.com>
2023-10-19 01:25:23 +0530

    Plus one code leetcode problem 66

--- plusOne.js
+++ plusOne.js
@@ -4,3 +4,23 @@
 // };
 
 // console.log(plusOne([1,2,3]))
+
+
+// OPTIMIZED METHOD
+
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        console.log("Iam inside", i)
+        if (digits[i] < 9) {
+            console.log("less than 9", digits[i])
+            digits[i]++;
+            console.log("AANOOP", digits)
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
+};
+
+console.log(plusOne([1,2,3]))
